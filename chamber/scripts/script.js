d4ded8001e01dd6e74aac9e891720bcb5690923e
Fabian Betancourt <fabianrbv@gmail.com>
2024-01-12 23:58:43 -0500

    new

---
+++ chamber/scripts/script.js
@@ -0,0 +1,24 @@
+/* JavaScript Dates (Intl) */
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+// Responsive 'Hamburger Menu'
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
