cccd0618b11ade42e1e40fef08512533778b7c2e
Fabian Betancourt <fabianrbv@gmail.com>
2024-01-13 23:49:21 -0500

    page

---
+++ scripts/getdates.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener el elemento span con el ID "currentYear"
+    var currentYearElement = document.getElementById("currentYear");
+
+    // Obtener el elemento p con el ID "lastModified"
+    var lastModifiedElement = document.getElementById("lastModified");
+
+    // Obtener el año actual y establecerlo en el span
+    var currentYear = new Date().getFullYear();
+    currentYearElement.textContent = currentYear;
+
+    // Obtener la fecha de última modificación del documento y establecerla en el párrafo
+    var lastModifiedDate = document.lastModified;
+    lastModifiedElement.textContent = "Last modified: " + lastModifiedDate;
+});
