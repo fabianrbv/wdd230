c23b4b3a931ef881946b4fc8771f0b8d0ad8a1ae
Fabian Betancourt <fabianrbv@gmail.com>
2024-02-22 18:25:00 -0500

    new

--- lesson07/scripts/bom.js
+++ lesson07/scripts/bom.js
@@ -2,31 +2,31 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', () => {
-    if (input.value !== '') {
-        const li = document.createElement('li');
+// button.addEventListener('click', () => {
+//     if (input.value !== '') {
+//         const li = document.createElement('li');
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = '❌';
+//         const deleteButton = document.createElement('button');
+//         deleteButton.textContent = '❌';
 
-        li.textContent = input.value;
+//         li.textContent = input.value;
 
-        li.appendChild(deleteButton);
+//         li.appendChild(deleteButton);
 
-        list.appendChild(li);
+//         list.appendChild(li);
 
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
-            input.focus();
-        });
+//         deleteButton.addEventListener('click', () => {
+//             list.removeChild(li);
+//             input.focus();
+//         });
 
-        input.focus();
+//         input.focus();
 
-        input.value = '';
-    } else {
-        alert('Please enter a book and chapter.');
-    }
-});
+//         input.value = '';
+//     } else {
+//         alert('Please enter a book and chapter.');
+//     }
+// });
 
 let chaptersArray = getChapterList() || [];
 
