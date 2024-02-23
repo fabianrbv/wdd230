a46b40fb26526a232c2a3f84de4e907a2403e871
Fabian Betancourt <fabianrbv@gmail.com>
2024-02-22 21:56:09 -0500

    actualizacion

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -22,3 +22,11 @@ document.addEventListener("DOMContentLoaded", function () {
 		hamButton.classList.toggle('open');
 	});
 });
+
+var toggle = document.getElementById("container");
+var body = document.querySelector("body");
+
+toggle.onclick = function () {
+	toggle.classList.toggle("active");
+	body.classList.toggle("active");
+}
--- lesson07/scripts/bom.js
+++ lesson07/scripts/bom.js
@@ -2,32 +2,6 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-// button.addEventListener('click', () => {
-//     if (input.value !== '') {
-//         const li = document.createElement('li');
-
-//         const deleteButton = document.createElement('button');
-//         deleteButton.textContent = '❌';
-
-//         li.textContent = input.value;
-
-//         li.appendChild(deleteButton);
-
-//         list.appendChild(li);
-
-//         deleteButton.addEventListener('click', () => {
-//             list.removeChild(li);
-//             input.focus();
-//         });
-
-//         input.focus();
-
-//         input.value = '';
-//     } else {
-//         alert('Please enter a book and chapter.');
-//     }
-// });
-
 let chaptersArray = getChapterList() || [];
 
 // Populate the displayed list of chapters using forEach
