511b3241ac1bfaf0700bf3e6380316c311404a0a
Fabian Betancourt <fabianrbv@gmail.com>
2024-03-07 04:35:50 -0500

    done

--- scripts/form.js
+++ scripts/form.js
@@ -2,13 +2,40 @@ function updateRatingValue(value) {
     document.getElementById('ratingValue').innerText = value;
 }
 
-const contactForm = document.getElementById('contactForm');
-const password = document.getElementById('password');
-const passwordConfirm = document.getElementById('passwordConfirm');
+const kp1 = document.querySelector("#password");
+const kp2 = document.querySelector("#passwordConfirm");
+const message = document.querySelector("#formmessage");
 
-contactForm.addEventListener('submit', (event) => {
-    if (password.value !== passwordConfirm.value) {
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+    if (kp1.value !== kp2.value) {
+        message.textContent = "❗Key Phrases DO NOT MATCH!";
+        message.style.visibility = "show";
+        kp2.style.backgroundColor = "#fff0f3";
+        kp2.value = "";
+        kp2.focus();
+    } else {
+        message.style.display = "none";
+        kp2.style.backgroundColor = "#fff";
+        kp2.style.color = "#000";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle form submission
+    function handleSubmit(event) {
+        // Prevent default form submission
         event.preventDefault();
-        document.getElementById('error').textContent = 'Passwords do not match.';
+
+        // Access form elements
+        let formt = event.target;
+        let formData = new FormData(formt);
+
+        // Display form element values
+        for (let pair of formData.entries()) {
+            console.log(pair[0] + ": " + pair[1]);
+        }
     }
 });
